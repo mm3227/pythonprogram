49ffddd1add5115257a045eb62ece0ee53a28b50
mm3227 <marioma3227@outlook.com>
2026-03-10 17:29:28 -0600

    actualización funcional usuarios
    se dejo funcional la pestaña usuarios

---
+++ Servidor/crear_db.py
@@ -0,0 +1,99 @@
+import os
+import sqlite3
+import hashlib
+import secrets
+
+RUTA_DB = "data/gestor.db"
+
+
+def hash_texto(texto):
+    return hashlib.sha256(texto.encode()).hexdigest()
+
+
+def crear_base_datos():
+
+    if not os.path.exists("data"):
+        os.makedirs("data")
+
+    conexion = sqlite3.connect(RUTA_DB)
+    cursor = conexion.cursor()
+
+    # -------------------------
+    # TABLA USUARIOS
+    # -------------------------
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario TEXT UNIQUE NOT NULL,
+        programa TEXT,
+        password TEXT NOT NULL,
+        tipo_usuario TEXT DEFAULT 'usuario'
+    )
+    """)
+
+    # -------------------------
+    # TABLA MATERIAS
+    # -------------------------
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS materias (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        programa TEXT,
+        materia TEXT,
+        continuidad TEXT,
+        creditos INTEGER
+    )
+    """)
+
+    # -------------------------
+    # TABLA PROFESORES
+    # -------------------------
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS profesores (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        programa TEXT,
+        nombre TEXT,
+        contratacion TEXT,
+        telefono TEXT,
+        email TEXT
+    )
+    """)
+
+    # -------------------------
+    # TABLA SALONES
+    # -------------------------
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS salones (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        programa TEXT,
+        edificio TEXT,
+        salon TEXT,
+        capacidad INTEGER
+    )
+    """)
+
+    # -------------------------
+    # VERIFICAR ADMIN
+    # -------------------------
+    usuario_admin = "admin"
+
+    cursor.execute("SELECT * FROM usuarios WHERE usuario=?", (usuario_admin,))
+    admin = cursor.fetchone()
+    admin_creado = False
+    password_plano = None
+
+    if not admin:
+
+        admin_creado = True
+        password_plano = secrets.token_hex(4)
+
+        password_hash = hash_texto(password_plano)
+
+        cursor.execute("""
+        INSERT INTO usuarios (usuario, programa, password, tipo_usuario)
+        VALUES (?, ?, ?, ?)
+        """, (usuario_admin, "sistema", password_hash, "admin"))
+
+    conexion.commit()
+    conexion.close()
+
+    return admin_creado, password_plano
--- Servidor/servidor_local.py
+++ Servidor/servidor_local.py
@@ -3,14 +3,16 @@ import socketserver
 import socket
 import sys
 import urllib.parse
+from crear_db import crear_base_datos
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+import hashlib
+import json
+import secrets
 
 PUERTO = 8000
-
-# usuario : {password, rol}
-USUARIOS = {
-    "admin": {"password": "1234", "rol": "admin"},
-    "mario": {"password": "uaz2025", "rol": "usuario"},
-}
+sesiones = {}
 
 
 def obtener_ip_local():
@@ -23,11 +25,88 @@ def obtener_ip_local():
     except:
         return "127.0.0.1"
 
+def verificar_usuario(usuario, password):
 
+    hash_password = hashlib.sha256(password.encode()).hexdigest()
+
+    conexion = sqlite3.connect("data/gestor.db")
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "SELECT tipo_usuario FROM usuarios WHERE usuario=? AND password=?",
+        (usuario, hash_password)
+    )
+
+    resultado = cursor.fetchone()
+    conexion.close()
+
+    if resultado:
+        return resultado[0]  # devuelve "admin" o "usuario"
+    else:
+        return None
+    
+def obtener_sesion(handler):
+    cookie = handler.headers.get("Cookie")
+    if not cookie:
+        return None
+    partes = cookie.split("=")
+    if len(partes) < 2:
+        return None
+    token = partes[1]
+    return sesiones.get(token)
+    
 class MiHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+
+        rol = obtener_sesion(self)
+
+        # proteger paginas de admin
+        if self.path.startswith("/admin"):
+
+            if rol != "admin":
+
+                self.send_response(403)
+                self.end_headers()
+                self.wfile.write(b"Acceso restringido")
+                return
+
+        # listar usuarios
+        if self.path == "/listar_usuarios":
+
+            conexion = sqlite3.connect("data/gestor.db")
+            cursor = conexion.cursor()
+
+            cursor.execute(
+                "SELECT id, usuario, programa, tipo_usuario FROM usuarios"
+            )
+
+            filas = cursor.fetchall()
+            conexion.close()
+
+            datos = []
+
+            for f in filas:
+                datos.append({
+                    "id": f[0],
+                    "usuario": f[1],
+                    "programa": f[2],
+                    "tipo_usuario": f[3]
+                })
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(datos).encode())
+
+        else:
+
+            super().do_GET()
 
     def do_POST(self):
-
+        # -------------------------
+        # LOGIN
+        # -------------------------
         if self.path == "/login":
 
             longitud = int(self.headers['Content-Length'])
@@ -38,16 +117,25 @@ class MiHandler(http.server.SimpleHTTPRequestHandler):
             usuario = campos.get("usuario", [""])[0]
             password = campos.get("password", [""])[0]
 
-            if usuario in USUARIOS and USUARIOS[usuario]["password"] == password:
+            rol = verificar_usuario(usuario, password)
 
-                rol = USUARIOS[usuario]["rol"]
+            if rol:
+
+                token = secrets.token_hex(16)
+                sesiones[token] = rol
 
                 if rol == "admin":
                     destino = "/admin.html"
                 else:
-                    destino = "/usuarios.html"
+                    destino = "/users.html"
 
                 self.send_response(302)
+
+                self.send_header(
+                    "Set-Cookie",
+                    f"session={token}; HttpOnly"
+                )
+
                 self.send_header("Location", destino)
                 self.end_headers()
 
@@ -56,29 +144,228 @@ class MiHandler(http.server.SimpleHTTPRequestHandler):
                 self.send_response(302)
                 self.send_header("Location", "/index.html?error=1")
                 self.end_headers()
+        
+
+        # -------------------------
+        # AGREGAR USUARIO
+        # -------------------------
+        elif self.path == "/agregar_usuario":
+
+            longitud = int(self.headers['Content-Length'])
+            datos = self.rfile.read(longitud).decode()
+
+            datos_json = json.loads(datos)
+
+            usuario = datos_json["usuario"]
+            programa = datos_json["programa"]
+            password = datos_json["password"]
+
+            # SOLO cifrar contraseña
+            hash_password = hashlib.sha256(password.encode()).hexdigest()
+
+            conexion = sqlite3.connect("data/gestor.db")
+            cursor = conexion.cursor()
+
+            try:
+
+                cursor.execute("""
+                INSERT INTO usuarios (usuario, programa, password)
+                VALUES (?, ?, ?)
+                """, (usuario, programa, hash_password))
+
+                conexion.commit()
+
+                respuesta = "Usuario agregado correctamente"
+
+            except sqlite3.IntegrityError:
+
+                respuesta = "El usuario ya existe"
+
+            conexion.close()
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(respuesta.encode())
+
+        # -------------------------
+        # IMPORTAR USUARIOS
+        # -------------------------
+        elif self.path == "/importar_usuarios":
+
+            import cgi
+            from openpyxl import load_workbook
+            import io
+
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={
+                    'REQUEST_METHOD': 'POST',
+                    'CONTENT_TYPE': self.headers['Content-Type']
+                }
+            )
+
+            archivo = form['archivo']
+
+            if not archivo.file:
+
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write(b"No se recibio archivo")
+                return
+
+            contenido = archivo.file.read()
+
+            archivo_excel = io.BytesIO(contenido)
+
+            wb = load_workbook(archivo_excel)
+            hoja = wb.active
+
+            conexion = sqlite3.connect("data/gestor.db")
+            cursor = conexion.cursor()
+
+            agregados = 0
+
+            for fila in hoja.iter_rows(min_row=2, values_only=True):
+
+                usuario, programa, password = fila
+
+                if not usuario:
+                    continue
+
+                hash_password = hashlib.sha256(str(password).encode()).hexdigest()
+
+                try:
+
+                    cursor.execute("""
+                    INSERT INTO usuarios (usuario, programa, password, tipo_usuario)
+                    VALUES (?, ?, ?, ?)
+                    """, (usuario, programa, hash_password, "usuario"))
+
+                    agregados += 1
+
+                except sqlite3.IntegrityError:
+                    pass
+
+            conexion.commit()
+            conexion.close()
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(f"{agregados} usuarios importados".encode())
+        # -------------------------
+        # Reiniciar Contraseñas
+        # -------------------------
+        elif self.path == "/reset_password":
+
+            longitud = int(self.headers['Content-Length'])
+            datos = self.rfile.read(longitud).decode()
+
+            datos_json = json.loads(datos)
+
+            user_id = datos_json["id"]
+
+            nueva_password = secrets.token_hex(4)
+
+            hash_password = hashlib.sha256(nueva_password.encode()).hexdigest()
+
+            conexion = sqlite3.connect("data/gestor.db")
+            cursor = conexion.cursor()
+
+            cursor.execute("""
+            UPDATE usuarios
+            SET password=?
+            WHERE id=?
+            """, (hash_password, user_id))
+
+            conexion.commit()
+            conexion.close()
+
+            respuesta = {
+                "password": nueva_password
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type","application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(respuesta).encode())
+        
+        # -------------------------
+        # Eliminar usuario
+        # -------------------------
+        elif self.path == "/eliminar_usuario":
+
+            longitud = int(self.headers['Content-Length'])
+            datos = self.rfile.read(longitud).decode()
+
+            datos_json = json.loads(datos)
+
+            user_id = datos_json["id"]
+
+            conexion = sqlite3.connect("data/gestor.db")
+            cursor = conexion.cursor()
+
+            # verificar tipo de usuario
+            cursor.execute("SELECT tipo_usuario FROM usuarios WHERE id=?", (user_id,))
+            tipo = cursor.fetchone()
+
+            # si no existe el usuario
+            if not tipo:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b"Usuario no encontrado")
+                conexion.close()
+                return
+
+            # bloquear eliminación del admin
+            if tipo[0] == "admin":
+
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(b"No se puede eliminar el administrador")
+                conexion.close()
+                return
+
+            # eliminar usuario
+            cursor.execute("DELETE FROM usuarios WHERE id=?", (user_id,))
+            conexion.commit()
+            conexion.close()
+
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Usuario eliminado")
+    
+def mostrar_info(ip_local, puerto, admin_creado, password):
+    root = tk.Tk()
+    root.withdraw()
+    mensaje = f"Servidor iniciado\n\n"
+    mensaje += f"Local:\nhttp://localhost:{puerto}\n\n"
+    mensaje += f"Red:\nhttp://{ip_local}:{puerto}\n"
+    if admin_creado:
+        mensaje += "\nADMIN CREADO\n"
+        mensaje += "usuario: admin\n"
+        mensaje += f"password: {password}\n"
+        mensaje += "\nGuarda esta contraseña."
+    messagebox.showinfo("Servidor Web", mensaje)
+    root.destroy()
 
 
 def main():
+    admin_creado, password = crear_base_datos()
 
     ip_local = obtener_ip_local()
 
-    print("="*50)
-    print("SERVIDOR WEB LOCAL")
-    print("="*50)
-    print(f"Local: http://localhost:{PUERTO}")
-    print(f"Red:   http://{ip_local}:{PUERTO}")
-    print("="*50)
+    mostrar_info(ip_local, PUERTO, admin_creado, password)
 
     handler = MiHandler
-    httpd = socketserver.TCPServer(("", PUERTO), handler)
+    httpd = socketserver.ThreadingTCPServer(("", PUERTO), handler)
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print("\nServidor detenido")
         httpd.shutdown()
         sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
