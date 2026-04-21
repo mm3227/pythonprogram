2c5b69c85752ce54654ea29d5eb96fa281bd1337
mm3227 <marioma3227@outlook.com>
2026-04-21 11:36:12 -0600

    se elimino todo
    se paso todo a otro repositorio

--- Servidor/config.py
+++
@@ -1,7 +0,0 @@
-DB_PATH = "data/gestor.db"
-BANNER_PATH = "/multimedia/banner.jpg"
-LOGO_PATH = "/multimedia/logo.jpg"
-FAVICON_PATH = "/multimedia/favicon-16x16.png"
-ESQUINA_PATH = "/multimedia/esquina.jpg"
-
-SESSION_TIMEOUT = 3600
